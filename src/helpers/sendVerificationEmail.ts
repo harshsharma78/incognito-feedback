06d9a65d896824191533194f8c0a0e69aba326be
Harsh <namastecoding@gmail.com>
2024-11-26 13:32:23 +0530

    add resend email for verification mails

---
+++ src/helpers/sendVerificationEmail.ts
@@ -0,0 +1,26 @@
+import VerificationEmail from "../../emails/VerificationEmail";
+import { resend } from "../lib/resend";
+import { ApiResponse } from "../types/ApiResponse";
+
+export async function sendVerificationEmail(
+  email: string,
+  username: string,
+  verifyCode: string,
+): Promise<ApiResponse> {
+  try {
+    await resend.emails.send({
+      from: "onboarding@resend.dev",
+      to: email,
+      subject: "Incognito Feedback | Verification Code",
+      react: VerificationEmail({ username, otp: verifyCode }),
+    });
+
+    return {
+      success: true,
+      message: "Verification email sent successfully",
+    };
+  } catch (emailError) {
+    console.error("Error while sending verification email", emailError);
+    return { success: false, message: "Failed to send verification email" };
+  }
+}
