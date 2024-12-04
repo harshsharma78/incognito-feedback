9b62a0e8457d37d462252e4b512bd43ab498519f
Harsh <namastecoding@gmail.com>
2024-12-04 16:32:35 +0530

    add account verification page

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -188,7 +188,7 @@ export default function SignIn() {
             Already a member?
             <Link
               href="/sign-in"
-              className="text-blue-600 hover:text-blue-800">
+              className="text-blue-600 hover:text-blue-800 ml-1">
               Sign in
             </Link>
           </p>
---
+++ src/app/verify/[username]/page.tsx
@@ -0,0 +1,90 @@
+"use client";
+import React from "react";
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../../../components/ui/form";
+import { Input } from "../../../components/ui/input";
+import { Button } from "../../../components/ui/button";
+import { useForm } from "react-hook-form";
+import { useParams, useRouter } from "next/navigation";
+import { useToast } from "../../../hooks/use-toast";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { verifySchema } from "../../../schemas/verifySchema";
+import { z } from "zod";
+import axios, { AxiosError } from "axios";
+import { ApiResponse } from "../../../types/ApiResponse";
+
+const VerifyAccount = () => {
+  const router = useRouter();
+  const params = useParams<{ username: string }>();
+  const { toast } = useToast();
+
+  const form = useForm<z.infer<typeof verifySchema>>({
+    resolver: zodResolver(verifySchema),
+  });
+
+  const onSubmit = async (data: z.infer<typeof verifySchema>) => {
+    try {
+      const response = await axios.post("/api/verify-code", {
+        username: params.username,
+        code: data.code,
+      });
+
+      toast({
+        title: "Success",
+        description: response.data.message,
+      });
+
+      router.replace("/sign-in");
+    } catch (error) {
+      console.error("Error in account verification", error);
+
+      const axiosError = error as AxiosError<ApiResponse>;
+
+      toast({
+        title: "Verification Failed",
+        description:
+          axiosError.response?.data.message ??
+          "An error occurred. Please try again.",
+        variant: "destructive",
+      });
+    }
+  };
+
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+        <div className="text-center">
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+            Verify Your Account
+          </h1>
+          <p className="mb-4">Enter the verification code sent to your email</p>
+        </div>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-6">
+            <FormField
+              name="code"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Verification Code</FormLabel>
+                  <Input {...field} />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Verify</Button>
+          </form>
+        </Form>
+      </div>
+    </div>
+  );
+};
+
+export default VerifyAccount;
